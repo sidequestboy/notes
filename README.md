18d46c79a23d9333ab285342ae0f111874a14b0f
jameh <jamie.alban@gmail.com>
2013-09-09 02:02:54 -0400

    Cleanup, add gh-pages as submodule.

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ notes
 =====
 
 Repository for notes taken from Queen's University.
+
+##How-to:
+Edit the source .rst files (in `source/`) with rst + mathjax syntax
+
+generate the html pages with
+```bash
+sphinx-build -b html ./ ./gh-pages
+```
+
+this will generate all the html in `gh-pages/` submodule. 
+
--- conf.py
+++ conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# reStucturedNotes documentation build configuration file, created by
-# sphinx-quickstart on Sat Sep  7 16:05:45 2013.
+# Jamie's reStructured Notes documentation build configuration file, created by
+# sphinx-quickstart on Mon Sep  9 01:57:39 2013.
 #
 # This file is execfile()d with the current directory set to its containing dir.
 #
@@ -25,10 +25,10 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.pngmath', 'sphinx.ext.mathjax']
+extensions = ['sphinx.ext.doctest', 'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['.templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'reStucturedNotes'
+project = u'Jamie\'s reStructured Notes'
 copyright = u'2013, Jamie Macdonald'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -64,7 +64,7 @@ release = '0.1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['.build']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
@@ -91,15 +91,15 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'flask'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'nosidebar': True}
+#html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['_themes']
+#html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -120,7 +120,7 @@ html_theme_path = ['_themes']
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['.static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
@@ -164,7 +164,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'reStucturedNotesdoc'
+htmlhelp_basename = 'JamiesreStructuredNotesdoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -183,7 +183,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'reStucturedNotes.tex', u'reStucturedNotes Documentation',
+  ('index', 'JamiesreStructuredNotes.tex', u'Jamie\'s reStructured Notes Documentation',
    u'Jamie Macdonald', 'manual'),
 ]
 
@@ -213,7 +213,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'restucturednotes', u'reStucturedNotes Documentation',
+    ('index', 'jamiesrestructurednotes', u'Jamie\'s reStructured Notes Documentation',
      [u'Jamie Macdonald'], 1)
 ]
 
@@ -227,8 +227,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'reStucturedNotes', u'reStucturedNotes Documentation',
-   u'Jamie Macdonald', 'reStucturedNotes', 'One line description of project.',
+  ('index', 'JamiesreStructuredNotes', u'Jamie\'s reStructured Notes Documentation',
+   u'Jamie Macdonald', 'JamiesreStructuredNotes', 'One line description of project.',
    'Miscellaneous'),
 ]
 
