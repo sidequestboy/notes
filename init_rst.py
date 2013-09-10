2d91d1e5039e2002bddaa099670c92f6eddcd6e6
jameh <jamie.alban@gmail.com>
2013-09-10 00:39:08 -0400

    Add python script that touches lecture note files for the day.

---
+++ init_rst.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+from time import strftime
+from sh import touch
+import os
+
+schedule = {"Monday": ["CISC333", "CMPE365", "MTHE474", "MTHE326", "MTHE455"],
+            "Tuesday": ["ELEC377", "MTHE494", "MTHE474", "MTHE455", "MTHE326"],
+            "Wednesday": ["CISC333", "CMPE365", "ELEC377", "MTHE455"],
+            "Thursday": ["ELEC377", "CISC333", "CMPE365", "MTHE494", "MTHE474",
+                         "MTHE473", "MTHE326"],
+            "Friday": ["CMPE365", "ELEC377", "MTHE494", "MTHE455", "CMPE365"]}
+
+if __name__ == '__main__':
+    for course in schedule[strftime("%A")]:
+        print("touching course: {}".format(course))
+        touch(os.path.join([course, strftime("%Y-%m-%d.rst")]))
